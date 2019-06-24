69344f5316f4d3349ca259f0ac23b6b93d1840d8
Phil Moir <pgmoir@gmail.com>
2019-06-24 07:11:04 +0100

    Change the error code from jwt errors to 401

--- controllers/auth.js
+++ controllers/auth.js
@@ -109,7 +109,7 @@ exports.login = (req, res, next) => {
       const token = jwt.sign({ 
           email: loadedUser.email,
           userId: loadedUser._id.toString()
-        }, JWT_SECRET, { expiresIn: '1h'}
+        }, JWT_SECRET, { expiresIn: '1h' }
       );
       res.status(200).json({ token: token, userId: loadedUser._id.toString() })
     })
--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   try {
     decodedToken = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    err.statusCode = 500;
+    err.statusCode = 401;
     throw err;
   }
   if (!decodedToken) {
