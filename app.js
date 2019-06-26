c2914c16bc291755823f607118eb06d83abc6b00
Phil Moir <pgmoir@gmail.com>
2019-06-26 15:23:37 +0100

    Added production url to cors

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000','https://youthkitbagweb.herokuapp.com'],
   optionsSuccessStatus: 200
 };
 
