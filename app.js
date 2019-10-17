508616860d50c0d408a9a922a4ea7e169f66b023
Phil Moir <pgmoir@gmail.com>
2019-10-17 19:21:38 +0100

    Add trust proxy to esnure that calls from Heroku to 3rd party authentication are done over https

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const MONGODB_URI = process.env.MONGODB_URL;
 
 const app = express();
 
+app.enable('trust proxy');
+
 aws.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
